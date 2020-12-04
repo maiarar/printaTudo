33032f673b9ca19187337a3e7fc47224f2e50fd6
Maiara Rodrigues <maiara.rodrigues@guiabolso.com>
2020-12-03 21:46:24 -0300

    added amazing files

---
+++ main.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send(`Bem vinde ao PrintaTudo! Mande qualquer requisição pra localhost:${port}!`)
+})
+
+app.post('/', (req, res) => {
+    console.log('chegou:\n', req.body);
+    res.sendStatus(200);
+  })
+
+app.listen(port, () => {
+  console.log(`Startado no http://localhost:${port}`)
+})
+
+module.exports = app
